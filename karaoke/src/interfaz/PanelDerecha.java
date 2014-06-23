74c0a1784975c78aa1301cec02b6cb84813584d1
MiguelAmaya <ankder15@gmail.com>
2014-06-22 20:24:21 -0500

    Agregar Componentes al panel

--- karaoke/src/interfaz/PanelDerecha.java
+++ karaoke/src/interfaz/PanelDerecha.java
@@ -6,14 +6,17 @@ public class PanelDerecha()extends JPanel{
     private JLabel  jlAnimacion, jlDescripcion;
     
     
-    public PanelDerech(){
+    public PanelDerecha(){
+      setSize(600,700);
     
     jlAnimacion = new JLabel();
     jlAnimacion.setSize(450,700);
+    setBackground(Color.RED);
     add(jlAnimacion);
     
     jlDescripcion = new JLabel();
     jlDescripcion.setSize(150,700);
+    setBackground(Color.BLUE);
     add(jlDescripcion);
     }
 
